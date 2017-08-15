0e781352b0d4c6d995bf67ff45fe492289b81963
MaxPlays <maxplays88@gmail.com>
2017-08-15 22:36:18 +0200

    HTTPS font query

--- web/index.php
+++ web/index.php
@@ -44,7 +44,7 @@
      <meta charset="utf-8">
      <title>Chatlog</title>
      <link rel="stylesheet" href="css/master.css">
-     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">
+     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">
      <link rel="icon" href="icon.png">
      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
    </head>
